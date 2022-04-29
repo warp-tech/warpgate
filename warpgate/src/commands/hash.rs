940f8975803af7cd66e09dc1c07f4aed490dcb2a
Eugene Pankov <e@ajenti.org>
2022-04-28 17:48:32 -0700

    replace isatty with atty

--- warpgate/src/commands/hash.rs
+++ warpgate/src/commands/hash.rs
@@ -1,13 +1,12 @@
 use anyhow::Result;
 use dialoguer::theme::ColorfulTheme;
-use isatty::stdin_isatty;
 use std::io::stdin;
 use warpgate_common::helpers::hash::hash_password;
 
 pub(crate) async fn command() -> Result<()> {
     let mut input = String::new();
 
-    if stdin_isatty() {
+    if atty::is(atty::Stream::Stdin) {
         input = dialoguer::Password::with_theme(&ColorfulTheme::default())
             .with_prompt("Password to be hashed")
             .interact()?;
