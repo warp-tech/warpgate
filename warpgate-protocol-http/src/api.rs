b1a784a5dfaf504c6764ffb91f113e50eecd8dc9
Eugene Pankov <e@ajenti.org>
2022-06-04 01:02:17 +0200

    wip

--- warpgate-protocol-http/src/api.rs
+++ warpgate-protocol-http/src/api.rs
@@ -1,12 +1,74 @@
-use http::uri::Authority;
+use std::borrow::Cow;
+use std::collections::HashSet;
+use std::str::FromStr;
+
+use delegate::delegate;
+use futures::{SinkExt, StreamExt};
+use http::header::HeaderName;
+use http::uri::{Authority, Scheme};
 use http::Uri;
-use poem::http::{HeaderValue, StatusCode};
+use poem::web::websocket::{CloseCode, Message, WebSocket};
 use poem::{handler, Body, IntoResponse, Request, Response};
+use tokio_tungstenite::{connect_async_with_config, tungstenite};
 use tracing::*;
 use warpgate_common::{Target, TargetHTTPOptions};
 
+trait SomeResponse {
+    fn status(&self) -> http::StatusCode;
+    fn headers(&self) -> &http::HeaderMap;
+}
+
+impl SomeResponse for reqwest::Response {
+    delegate! {
+        to self {
+            fn status(&self) -> http::StatusCode;
+            fn headers(&self) -> &http::HeaderMap;
+        }
+    }
+}
+
+impl<B> SomeResponse for http::Response<B> {
+    delegate! {
+        to self {
+            fn status(&self) -> http::StatusCode;
+            fn headers(&self) -> &http::HeaderMap;
+        }
+    }
+}
+
+trait SomeRequestBuilder {
+    fn header(self, k: HeaderName, v: String) -> Self;
+}
+
+impl SomeRequestBuilder for reqwest::RequestBuilder {
+    delegate! {
+        to self {
+            fn header(self, k: HeaderName, v: String) -> Self;
+        }
+    }
+}
+
+impl SomeRequestBuilder for http::request::Builder {
+    delegate! {
+        to self {
+            fn header(self, k: HeaderName, v: String) -> Self;
+        }
+    }
+}
+
 #[handler]
-pub async fn test_endpoint(req: &Request, body: Body) -> poem::Result<Response> {
+pub async fn test_endpoint(
+    req: &Request,
+    ws: Option<WebSocket>,
+    body: Body,
+) -> poem::Result<Response> {
+    Ok(match ws {
+        Some(ws) => proxy_ws(req, ws).await?.into_response(),
+        None => proxy_normal(req, body).await?.into_response(),
+    })
+}
+
+pub async fn proxy_normal(req: &Request, body: Body) -> poem::Result<Response> {
     let mut res = String::new();
     let mut has_auth = false;
     for h in req.headers().iter() {
@@ -37,30 +99,98 @@ pub async fn test_endpoint(req: &Request, body: Body) -> poem::Result<Response> 
     // Ok(r)
 }
 
-async fn proxy_request(req: &Request, body: Body) -> poem::Result<Response> {
-    let target = Target {
+lazy_static::lazy_static! {
+    static ref DEMO_TARGET: Target = Target {
         allow_roles: vec![],
         http: Some(TargetHTTPOptions {
             url: "http://192.168.78.233/".to_string(),
         }),
-        name: "Target".to_string(),
+        name: String::from("Target"),
         ssh: None,
         web_admin: None,
     };
 
-    let target_uri = Uri::try_from(target.http.unwrap().url).unwrap();
+    static ref DONT_FORWARD_HEADERS: HashSet<HeaderName> = {
+        let mut s = HashSet::new();
+        s.insert("sec-websocket-extensions".parse().unwrap());
+        s.insert("sec-websocket-accept".parse().unwrap());
+        s.insert("sec-websocket-key".parse().unwrap());
+        s.insert("sec-websocket-version".parse().unwrap());
+        s.insert("upgrade".parse().unwrap());
+        s.insert("connection".parse().unwrap());
+        s.insert("strict-transport-security".parse().unwrap());
+        s
+    };
+}
+
+fn construct_uri(req: &Request, target: &Target, websocket: bool) -> Uri {
+    let target_uri = Uri::try_from(target.http.clone().unwrap().url).unwrap();
     let source_uri = req.uri().clone();
 
     let authority = target_uri.authority().unwrap().to_string();
     let authority = authority.split("@").last().unwrap();
     let authority: Authority = authority.try_into().unwrap();
-    let uri = http::uri::Builder::new()
+    let mut uri = http::uri::Builder::new()
         .authority(authority)
-        .path_and_query(source_uri.path_and_query().unwrap().clone())
-        .scheme(target_uri.scheme().unwrap().clone())
-        .build()
-        .unwrap()
-        .to_string();
+        .path_and_query(source_uri.path_and_query().unwrap().clone());
+
+    uri = uri.scheme(target_uri.scheme().unwrap().clone());
+
+    if websocket {
+        uri = uri.scheme(
+            Scheme::from_str(
+                if target_uri.scheme().unwrap() == &Scheme::from_str("http").unwrap() {
+                    "ws"
+                } else {
+                    "wss"
+                },
+            )
+            .unwrap(),
+        );
+    }
+
+    uri.build().unwrap()
+}
+
+fn copy_client_response<R: SomeResponse>(
+    client_response: &R,
+    server_response: &mut poem::Response,
+) {
+    let mut headers = client_response.headers().clone();
+    for h in client_response.headers().iter() {
+        if DONT_FORWARD_HEADERS.contains(h.0) {
+            if let http::header::Entry::Occupied(e) = headers.entry(h.0) {
+                e.remove_entry();
+            }
+        }
+    }
+    server_response.headers_mut().extend(headers.into_iter());
+
+    server_response.set_status(client_response.status());
+}
+
+fn copy_server_request<B: SomeRequestBuilder>(req: &Request, mut target: B) -> B {
+    for k in req.headers().keys() {
+        if DONT_FORWARD_HEADERS.contains(k) {
+            continue;
+        }
+        target = target.header(
+            k.clone(),
+            req.headers()
+                .get_all(k)
+                .iter()
+                .map(|v| v.to_str().unwrap().to_string())
+                .collect::<Vec<_>>()
+                .join("; "),
+        );
+    }
+    target
+}
+
+async fn proxy_request(req: &Request, body: Body) -> poem::Result<Response> {
+    let target = DEMO_TARGET.clone();
+
+    let uri = construct_uri(req, &target, false).to_string();
 
     tracing::debug!("URI: {:?}", uri);
 
@@ -71,17 +201,7 @@ async fn proxy_request(req: &Request, body: Body) -> poem::Result<Response> {
         .unwrap();
     let mut client_request = client.request(req.method().into(), uri.clone());
 
-    for k in req.headers().keys() {
-        client_request = client_request.header(
-            k.clone(),
-            req.headers()
-                .get_all(k)
-                .iter()
-                .map(|v| v.to_str().unwrap().to_string())
-                .collect::<Vec<_>>()
-                .join("; "),
-        );
-    }
+    client_request = copy_server_request(&req, client_request);
 
     client_request = client_request.body(reqwest::Body::wrap_stream(body.into_bytes_stream()));
 
@@ -90,10 +210,6 @@ async fn proxy_request(req: &Request, body: Body) -> poem::Result<Response> {
 
     let mut response: Response = "".into();
 
-    response
-        .headers_mut()
-        .extend(client_response.headers().clone().into_iter());
-
     tracing::info!(
         "{:?} {:?} - {:?}",
         client_response.status(),
@@ -101,8 +217,127 @@ async fn proxy_request(req: &Request, body: Body) -> poem::Result<Response> {
         client_response.content_length().unwrap_or(0)
     );
 
-    response.set_status(client_response.status());
+    copy_client_response(&client_response, &mut response);
     response.set_body(Body::from_bytes_stream(client_response.bytes_stream()));
+
+    Ok(response)
+}
+
+async fn proxy_ws(req: &Request, ws: WebSocket) -> poem::Result<impl IntoResponse> {
+    let target = DEMO_TARGET.clone();
+    let uri = construct_uri(req, &target, true);
+    proxy_ws_inner(req, ws, uri.clone()).await.map_err(|error| {
+        tracing::error!(?uri, ?error, "WebSocket proxy failed");
+        error
+    })
+}
+
+async fn proxy_ws_inner(req: &Request, ws: WebSocket, uri: Uri) -> poem::Result<impl IntoResponse> {
+    let mut client_request = http::request::Builder::new()
+        .uri(uri.clone())
+        .header("Connection", "Upgrade")
+        .header("Upgrade", "websocket")
+        .header("Sec-WebSocket-Version", "13")
+        .header(
+            "Sec-WebSocket-Key",
+            tungstenite::handshake::client::generate_key(),
+        );
+    client_request = copy_server_request(&req, client_request);
+
+    let (client, client_response) = connect_async_with_config(
+        client_request
+            .body(())
+            .map_err(poem::error::InternalServerError)?,
+        None,
+    )
+    .await
+    .map_err(poem::error::BadGateway)?;
+
+    tracing::info!("{:?} {:?} - WebSocket", client_response.status(), uri);
+
+    let mut response = ws
+        .on_upgrade(|socket| async move {
+            let (mut client_sink, mut client_source) = client.split();
+
+            let (mut server_sink, mut server_source) = socket.split();
+
+            if let Err(error) = {
+                let server_to_client = tokio::spawn(async move {
+                    while let Some(msg) = server_source.next().await {
+                        tracing::debug!("Server: {:?}", msg);
+                        match msg? {
+                            Message::Binary(data) => {
+                                client_sink.send(tungstenite::Message::Binary(data)).await?;
+                            }
+                            Message::Text(text) => {
+                                client_sink.send(tungstenite::Message::Text(text)).await?;
+                            }
+                            Message::Ping(data) => {
+                                client_sink.send(tungstenite::Message::Ping(data)).await?;
+                            }
+                            Message::Pong(data) => {
+                                client_sink.send(tungstenite::Message::Pong(data)).await?;
+                            }
+                            Message::Close(data) => {
+                                client_sink
+                                    .send(tungstenite::Message::Close(data.map(|data| {
+                                        tungstenite::protocol::CloseFrame {
+                                            code: data.0.into(),
+                                            reason: Cow::Owned(data.1),
+                                        }
+                                    })))
+                                    .await?;
+                            }
+                        }
+                    }
+                    Ok::<_, anyhow::Error>(())
+                });
+
+                let client_to_server = tokio::spawn(async move {
+                    while let Some(msg) = client_source.next().await {
+                        tracing::debug!("Client: {:?}", msg);
+                        match msg? {
+                            tungstenite::Message::Binary(data) => {
+                                server_sink.send(Message::Binary(data)).await?;
+                            }
+                            tungstenite::Message::Text(text) => {
+                                server_sink.send(Message::Text(text)).await?;
+                            }
+                            tungstenite::Message::Ping(data) => {
+                                server_sink.send(Message::Ping(data)).await?;
+                            }
+                            tungstenite::Message::Pong(data) => {
+                                server_sink.send(Message::Pong(data)).await?;
+                            }
+                            tungstenite::Message::Close(data) => {
+                                server_sink
+                                    .send(Message::Close(data.map(|data| {
+                                        (
+                                            CloseCode::from(data.code),
+                                            data.reason.to_owned().to_string(),
+                                        )
+                                    })))
+                                    .await?;
+                            }
+                            tungstenite::Message::Frame(_) => unreachable!(),
+                        }
+                    }
+                    Ok::<_, anyhow::Error>(())
+                });
+
+                server_to_client.await??;
+                client_to_server.await??;
+                debug!("Closing Websocket stream");
+
+                Ok::<_, anyhow::Error>(())
+            } {
+                error!(?error, "Websocket stream error");
+            }
+            Ok::<_, anyhow::Error>(())
+        })
+        .into_response();
+
+    copy_client_response(&client_response, &mut response);
 
     Ok(response)
 }
