0e691e7330847a967ccdc30ab114506c95a6c938
Eugene Pankov <e@ajenti.org>
2022-06-06 15:42:33 +0200

    Update main.embed.ts

--- warpgate-web/src/main.embed.ts
+++ warpgate-web/src/main.embed.ts
@@ -1,3 +1,9 @@
+navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (let registration of registrations) {
+        registration.unregister()
+    }
+})
+
 console.log('Embedded!')
 
-export {}
+export { }
