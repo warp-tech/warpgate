60a7c08fb1d27024c0b0c9d3d09cbe0df35c78bd
Eugene Pankov <e@ajenti.org>
2022-06-26 21:28:49 +0200

    bumped totp-rs

--- warpgate-common/src/helpers/otp.rs
+++ warpgate-common/src/helpers/otp.rs
@@ -14,12 +14,20 @@ pub fn generate_key() -> OtpSecretKey {
 }
 
 pub fn generate_setup_url(key: &OtpSecretKey, label: &str) -> Secret<String> {
-    let totp = get_totp(key);
-    Secret::new(totp.get_url(label, "Warpgate"))
+    let totp = get_totp(key, Some(label));
+    Secret::new(totp.get_url())
 }
 
-fn get_totp(key: &OtpSecretKey) -> TOTP<OtpExposedSecretKey> {
-    TOTP::new(Algorithm::SHA1, 6, 1, 30, key.expose_secret().clone())
+fn get_totp(key: &OtpSecretKey, label: Option<&str>) -> TOTP<OtpExposedSecretKey> {
+    TOTP {
+        algorithm: Algorithm::SHA1,
+        digits: 6,
+        skew: 1,
+        step: 30,
+        secret: key.expose_secret().clone(),
+        issuer: Some("Warpgate".to_string()),
+        account_name: label.unwrap_or("").to_string(),
+    }
 }
 
 pub fn verify_totp(code: &str, key: &OtpSecretKey) -> bool {
@@ -27,5 +35,5 @@ pub fn verify_totp(code: &str, key: &OtpSecretKey) -> bool {
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap()
         .as_secs();
-    get_totp(key).check(code, time)
+    get_totp(key, None).check(code, time)
 }
