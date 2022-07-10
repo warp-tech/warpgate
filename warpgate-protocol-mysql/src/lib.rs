0ce8998a7db5d882250560130062874ccbd1aa00
Eugene Pankov <e@ajenti.org>
2022-07-10 18:04:34 +0200

    wip

--- warpgate-protocol-mysql/src/lib.rs
+++ warpgate-protocol-mysql/src/lib.rs
@@ -3,25 +3,28 @@ mod common;
 use anyhow::{Context, Result};
 use async_trait::async_trait;
 use bytes::buf::Chain;
-use bytes::{Buf, Bytes, BytesMut};
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 use mysql_common::constants::{CapabilityFlags, StatusFlags};
 use mysql_common::io::ParseBuf;
+use mysql_common::misc::raw::bytes::BareBytes;
 use mysql_common::misc::raw::{Const, RawBytes, RawInt, Skip};
-use mysql_common::packets::{HandshakePacket, HandshakeResponse, OkPacket};
+use mysql_common::packets::{AuthSwitchRequest, HandshakePacket, HandshakeResponse};
 use mysql_common::proto::codec::PacketCodec;
 use mysql_common::proto::{MyDeserialize, MySerialize};
-use sqlx_core_guts::io::{BufStream, Encode};
-use sqlx_core_guts::mysql::connection::stream::MySqlStream;
-use sqlx_core_guts::mysql::protocol::connect::Handshake;
-use sqlx_core_guts::mysql::protocol::response::Status;
-use sqlx_core_guts::mysql::protocol::{Capabilities, Packet};
+use rand::Rng;
+use sha1::Digest;
+use sqlx_core_guts::io::BufStream;
+use sqlx_core_guts::mysql::io::MySqlBufMutExt;
+use sqlx_core_guts::mysql::protocol::response::{ErrPacket, OkPacket, Status};
 use std::borrow::Cow;
 use std::fmt::Debug;
+use std::marker::PhantomData;
 use std::net::SocketAddr;
 use tokio::io::{AsyncRead, AsyncReadExt};
 use tokio::net::tcp::ReadHalf;
 use tokio::net::{TcpListener, TcpStream};
 use tracing::*;
+use warpgate_common::helpers::rng::get_crypto_rng;
 use warpgate_common::{ProtocolServer, Services, Target, TargetTestError};
 
 pub struct MySQLProtocolServer {
@@ -66,94 +69,52 @@ impl Debug for MySQLProtocolServer {
 
 struct Session {
     stream: BufStream<TcpStream>,
-    sequence_id: u8,
-    capabilities: Capabilities,
+    codec: PacketCodec,
+    capabilities: CapabilityFlags,
+    inbound_buffer: BytesMut,
+    outbound_buffer: BytesMut,
+}
+
+pub trait SerializePacket {
+    fn serialize(&self, buf: &mut Vec<u8>);
+}
+
+impl SerializePacket for OkPacket {
+    fn serialize(&self, buf: &mut Vec<u8>) {
+        buf.put_u8(0);
+        buf.put_uint_lenenc(self.affected_rows);
+        buf.put_uint_lenenc(self.last_insert_id);
+        buf.put_u16_le(self.status.bits());
+        buf.put_u16_le(self.warnings);
+    }
+}
+
+impl SerializePacket for ErrPacket {
+    fn serialize(&self, buf: &mut Vec<u8>) {
+        buf.put_u8(0xff);
+        buf.put_u16_le(self.error_code);
+        buf.extend_from_slice(self.error_message.as_bytes())
+        //TODO: sql_state
+    }
+}
+
+impl SerializePacket for HandshakePacket<'_> {
+    fn serialize(&self, buf: &mut Vec<u8>) {
+        MySerialize::serialize(self, buf);
+    }
+}
+
+impl SerializePacket for AuthSwitchRequest<'_> {
+    fn serialize(&self, buf: &mut Vec<u8>) {
+        MySerialize::serialize(self, buf);
+    }
 }
 
 impl Session {
     pub fn new(stream: TcpStream) -> Self {
-        let mut capabilities = Capabilities::PROTOCOL_41
-            | Capabilities::IGNORE_SPACE
-            | Capabilities::DEPRECATE_EOF
-            | Capabilities::FOUND_ROWS
-            | Capabilities::TRANSACTIONS
-            | Capabilities::SECURE_CONNECTION
-            | Capabilities::PLUGIN_AUTH_LENENC_DATA
-            | Capabilities::MULTI_STATEMENTS
-            | Capabilities::MULTI_RESULTS
-            | Capabilities::PLUGIN_AUTH
-            | Capabilities::PS_MULTI_RESULTS
-            | Capabilities::SSL;
-
         Self {
             stream: BufStream::new(stream),
-            capabilities,
-            sequence_id: 0,
-        }
-    }
-
-    async fn recv_packet(&mut self) -> Result<Packet<Bytes>> {
-        // https://dev.mysql.com/doc/dev/mysql-server/8.0.12/page_protocol_basic_packets.html
-        // https://mariadb.com/kb/en/library/0-packet/#standard-packet
-
-        let mut header: Bytes = self.stream.read(4).await?;
-
-        let packet_size = header.get_uint_le(3) as usize;
-        let sequence_id = header.get_u8();
-
-        self.sequence_id = sequence_id.wrapping_add(1);
-
-        let payload: Bytes = self.stream.read(packet_size).await?;
-
-        // TODO: packet compression
-        // TODO: packet joining
-
-        if payload
-            .get(0)
-            .ok_or(anyhow::anyhow!("Packet empty"))?
-            .eq(&0xff)
-        {
-            // self.waiting.pop_front();
-
-            // instead of letting this packet be looked at everywhere, we check here
-            // and emit a proper Error
-            anyhow::bail!("Protocol error");
-            // return Err(MySqlDatabaseError(ErrPacket::decode_with(
-            //     payload,
-            //     self.capabilities,
-            // )?)
-            // .into());
-        }
-
-        Ok(Packet(payload))
-    }
-
-    pub(crate) async fn send_packet<'en, T>(&mut self, payload: T) -> Result<()>
-    where
-        T: Encode<'en, Capabilities>,
-    {
-        self.sequence_id = 0;
-        self.write_packet(payload);
-        self.stream.flush().await.context("Failed to flush stream")
-    }
-
-    pub(crate) fn write_packet<'en, T>(&mut self, payload: T)
-    where
-        T: Encode<'en, Capabilities>,
-    {
-        self.stream
-            .write_with(Packet(payload), (self.capabilities, &mut self.sequence_id));
-    }
-
-    pub async fn run(mut self) -> Result<()> {
-        let mut inner = vec![];
-        HandshakePacket::new(
-            10,
-            Cow::Borrowed(&b"10.0.0-Warpgate"[..]),
-            1,
-            b"abcdefgh".to_owned(),
-            None::<&[u8]>,
-            CapabilityFlags::CLIENT_PROTOCOL_41
+            capabilities: CapabilityFlags::CLIENT_PROTOCOL_41
                 | CapabilityFlags::CLIENT_PLUGIN_AUTH
                 | CapabilityFlags::CLIENT_LONG_PASSWORD
                 | CapabilityFlags::CLIENT_FOUND_ROWS
@@ -175,42 +136,125 @@ impl Session {
                 | CapabilityFlags::CLIENT_DEPRECATE_EOF
                 | CapabilityFlags::CLIENT_RESERVED
                 | CapabilityFlags::CLIENT_SECURE_CONNECTION,
-            0,
-            StatusFlags::empty(),
-            Some(Cow::Borrowed(&b"mysql_native_password"[..])),
-        )
-        .serialize(&mut inner);
+            codec: PacketCodec::default(),
+            inbound_buffer: BytesMut::new(),
+            outbound_buffer: BytesMut::new(),
+        }
+    }
 
-        let mut packet = BytesMut::new();
-        let mut codec = PacketCodec::default();
-        codec
-            .encode(&mut &*inner, &mut packet)
-            .context("Failed to encode handshake packet")?;
+    fn push<P: SerializePacket>(&mut self, packet: &P) -> Result<()> {
+        let mut buf = vec![];
+        packet.serialize(&mut buf);
+        self.codec
+            .encode(&mut &*buf, &mut self.outbound_buffer)
+            .context("Failed to encode packet")?;
+        Ok(())
+    }
 
-        self.stream.write(&packet[..]);
-
+    async fn flush(&mut self) -> Result<()> {
+        trace!(outbound_buffer=?self.outbound_buffer, "flushing");
+        self.stream.write(&self.outbound_buffer[..]);
+        self.outbound_buffer = BytesMut::new();
         self.stream
             .flush()
             .await
             .context("Failed to flush stream")?;
+        Ok(())
+    }
 
-        let mut inbound_buffer = BytesMut::new();
+    // async fn recv<'a, P>(
+    //     &'a mut self,
+    //     ctx: P::Ctx,
+    // ) -> Result<P> where P: MyDeserialize<'a> {    }
+
+    async fn recv(&mut self) -> Result<Vec<u8>> {
+        let mut payload = vec![];
         loop {
-            let read_bytes = self.stream.read_buf(&mut inbound_buffer).await?;
+            let read_bytes = self.stream.read_buf(&mut self.inbound_buffer).await?;
             if read_bytes == 0 {
-                break;
+                anyhow::bail!("Unexpected EOF");
             }
-            info!(?inbound_buffer, "got packet");
-            let mut inner_buf = vec![];
-            let result = codec.decode(&mut inbound_buffer, &mut inner_buf)?;
-            if result {
-                info!(?inner_buf, "got full packet");
-                let mut pb = ParseBuf(&inner_buf);
-                let pkt = HandshakeResponse::deserialize((), &mut pb)?;
-                info!(?pkt, "got response");
+            trace!(inbound_buffer=?self.inbound_buffer, "chunk");
+            {
+                let got_full_packet = self.codec.decode(&mut self.inbound_buffer, &mut payload)?;
+                if got_full_packet {
+                    break;
+                }
             }
-            info!(?inbound_buffer, "after got packet");
         }
+        trace!(inbound_buffer=?self.inbound_buffer, "after packet");
+        Ok(payload)
+        // let result = P::deserialize(ctx, &mut pb);
+        // drop(pb);
+        // return result.context("Failed to deserialize");
+    }
+
+    pub async fn run(mut self) -> Result<()> {
+        let handshake = HandshakePacket::new(
+            10,
+            Cow::Borrowed(&b"10.0.0-Warpgate"[..]),
+            1,
+            b"abcdefgh".to_owned(),
+            None::<&[u8]>,
+            self.capabilities,
+            0,
+            StatusFlags::empty(),
+            Some(&b"mysql_native_password"[..]),
+        );
+        self.push(&handshake)?;
+        self.flush().await?;
+
+        let payload = self.recv().await?;
+        let resp = ParseBuf(&payload)
+            .parse::<HandshakeResponse>(())
+            .context("Failed to parse packet")?;
+        info!(?resp, "got response");
+
+        let challenge = get_crypto_rng().gen::<[u8; 20]>();
+        self.push(&AuthSwitchRequest::new(
+            &b"mysql_native_password"[..],
+            &challenge[..],
+        ))?;
+        // self.push(&RawBytes::<
+        self.flush().await?;
+
+        let client_response = password_hash::Output::new(&self.recv().await?);
+        let expected_response = password_hash::Output::new(
+            &{
+                let true_password = b"123";
+                let password_sha: [u8; 20] = sha1::Sha1::digest(true_password).into();
+                let password_sha_sha: [u8; 20] = sha1::Sha1::digest(password_sha).into();
+                let password_seed_2sha_sha: [u8; 20] =
+                    sha1::Sha1::digest([challenge, password_sha_sha].concat()).into();
+
+                let mut result = password_sha;
+                result
+                    .iter_mut()
+                    .zip(password_seed_2sha_sha.iter())
+                    .for_each(|(x1, x2)| *x1 ^= *x2);
+                result
+            }[..],
+        );
+
+        info!(?client_response, "client_response");
+        info!(?expected_response, "exp response");
+        info!("correct {}", client_response == expected_response);
+
+        if client_response == expected_response {
+            self.push(&OkPacket {
+                affected_rows: 0,
+                last_insert_id: 0,
+                status: Status::empty(),
+                warnings: 0,
+            })?;
+        } else {
+            self.push(&ErrPacket {
+                error_code: 0,
+                error_message: "Access denied".to_owned(),
+                sql_state: None,
+            })?;
+        }
+
         Ok(())
     }
 }
