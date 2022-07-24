328e8f0ac559ef63f6bf61e5c6bc978f3a7cf200
Eugene Pankov <e@ajenti.org>
2022-07-24 15:43:20 +0200

    wip

--- e2e/test_main.py
+++ e2e/test_main.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import psutil
+import pytest
 import shutil
 import socket
 import subprocess
@@ -10,16 +12,9 @@ from pathlib import Path
 from textwrap import dedent
 from typing import List
 
-children: List[subprocess.Popen] = []
-
 last_port = 1234
 
 cargo_root = Path(os.getcwd()).parent
-
-
-@dataclass
-class Context:
-    tmpdir: Path
 
 
 def alloc_port():
@@ -33,6 +28,7 @@ def wait_port(port):
     while True:
         try:
             s = socket.create_connection(('localhost', port))
+            s.recv(1)
             s.close()
             logging.debug(f'Port {port} is up')
             break
@@ -41,167 +37,249 @@ def wait_port(port):
             continue
 
 
-def start_ssh_server(ctx: Context, trusted_keys=[]):
-    port = alloc_port()
-    data_dir = ctx.tmpdir / 'sshd'
-    data_dir.mkdir(parents=True)
-    authorized_keys_path = data_dir / 'authorized_keys'
-    authorized_keys_path.write_text('\n'.join(trusted_keys))
-    config_path = data_dir / 'sshd_config'
-    config_path.write_text(
-        dedent(
-            f'''\
-            Port {port}
-            AuthorizedKeysFile {authorized_keys_path}
-            AllowAgentForwarding yes
-            AllowTcpForwarding yes
-            GatewayPorts yes
-            X11Forwarding yes
-            UseDNS no
-            PermitTunnel yes
-            StrictModes no
-            UsePam no
-            HostKey {os.getcwd()}/ssh-keys/id_ed25519
-            Subsystem	sftp	/usr/lib/ssh/sftp-server
-            '''
+@dataclass
+class Context:
+    tmpdir: Path
+
+
+class ProcessManager:
+    children: List[subprocess.Popen]
+
+    def __init__(self, ctx: Context) -> None:
+        self.children = []
+        self.ctx = ctx
+
+    def stop(self):
+        for child in self.children:
+            try:
+                p = psutil.Process(child.pid)
+            except psutil.NoSuchProcess:
+                continue
+
+            for sp in p.children(recursive=True):
+                try:
+                    sp.terminate()
+                except psutil.NoSuchProcess:
+                    pass
+
+            p.terminate()
+
+            try:
+                p.wait(timeout=3)
+            except psutil.TimeoutExpired:
+                for sp in p.children(recursive=True):
+                    try:
+                        sp.kill()
+                    except psutil.NoSuchProcess:
+                        pass
+
+    def start_ssh_server(self, trusted_keys=[]):
+        port = alloc_port()
+        data_dir = self.ctx.tmpdir / 'sshd'
+        data_dir.mkdir(parents=True)
+        authorized_keys_path = data_dir / 'authorized_keys'
+        authorized_keys_path.write_text('\n'.join(trusted_keys))
+        config_path = data_dir / 'sshd_config'
+        config_path.write_text(
+            dedent(
+                f'''\
+                Port 22
+                AuthorizedKeysFile {authorized_keys_path}
+                AllowAgentForwarding yes
+                AllowTcpForwarding yes
+                GatewayPorts yes
+                X11Forwarding yes
+                UseDNS no
+                PermitTunnel yes
+                StrictModes no
+                PermitRootLogin yes
+                HostKey /ssh-keys/id_ed25519
+                Subsystem	sftp	/usr/lib/ssh/sftp-server
+                '''
+            )
         )
-    )
-    data_dir.chmod(0o700)
-    authorized_keys_path.chmod(0o600)
-    config_path.chmod(0o600)
+        data_dir.chmod(0o700)
+        authorized_keys_path.chmod(0o600)
+        config_path.chmod(0o600)
 
-    p = subprocess.Popen(['/usr/sbin/sshd', '-f', str(config_path), '-De'])
-    children.append(p)
-    return port
-
-
-def start_wg(ctx: Context, config=''):
-    port = alloc_port()
-    data_dir = ctx.tmpdir / 'wg-data'
-    data_dir.mkdir(parents=True)
-    keys_dir = data_dir / 'keys'
-    keys_dir.mkdir(parents=True)
-    for k in [
-        Path('ssh-keys/wg/client-ed25519'),
-        Path('ssh-keys/wg/client-rsa'),
-        Path('ssh-keys/wg/host-ed25519'),
-        Path('ssh-keys/wg/host-rsa'),
-    ]:
-        shutil.copy(k, keys_dir / k.name)
-    config_path = data_dir / 'warpgate.yaml'
-    config_path.write_text(
-        dedent(
-            f'''\
-            ssh:
-                enable: true
-                listen: 0.0.0.0:{port}
-                keys: {keys_dir}
-                host_key_verification: auto_accept
-            http:
-                enable: false
-            mysql:
-                enable: false
-            recordings:
-                enable: false
-            roles:
-            - name: role
-            http:
-                enable: false
-            '''
+        p = subprocess.Popen(
+            [
+                'docker',
+                'run',
+                '--rm',
+                '-p',
+                f'{port}:22',
+                '-v',
+                f'{data_dir}:{data_dir}',
+                '-v',
+                f'{os.getcwd()}/ssh-keys:/ssh-keys',
+                'warpgate-e2e-ssh-server',
+                '-f',
+                str(config_path),
+            ]
         )
-        + config
-    )
-    p = subprocess.Popen(
-        [
-            'cargo',
-            'llvm-cov',
-            'run',
-            '--no-report',
-            '--',
-            '--config',
-            str(config_path),
-            'run',
-        ],
-        cwd=cargo_root,
-    )
-    children.append(p)
-    return port
+        self.children.append(p)
+        return port
+
+    def start_wg(self, config='', args=None):
+        port = alloc_port()
+        data_dir = self.ctx.tmpdir / 'wg-data'
+        data_dir.mkdir(parents=True)
+        keys_dir = data_dir / 'keys'
+        keys_dir.mkdir(parents=True)
+        for k in [
+            Path('ssh-keys/wg/client-ed25519'),
+            Path('ssh-keys/wg/client-rsa'),
+            Path('ssh-keys/wg/host-ed25519'),
+            Path('ssh-keys/wg/host-rsa'),
+        ]:
+            shutil.copy(k, keys_dir / k.name)
+        config_path = data_dir / 'warpgate.yaml'
+        config_path.write_text(
+            dedent(
+                f'''\
+                ssh:
+                    enable: true
+                    listen: 0.0.0.0:{port}
+                    keys: {keys_dir}
+                    host_key_verification: auto_accept
+                http:
+                    enable: false
+                mysql:
+                    enable: false
+                recordings:
+                    enable: false
+                roles:
+                - name: role
+                http:
+                    enable: false
+                '''
+            )
+            + config
+        )
+        args = args or ['run']
+        p = subprocess.Popen(
+            [
+                'cargo',
+                'llvm-cov',
+                'run',
+                '--no-report',
+                '--',
+                '--config',
+                str(config_path),
+                *args,
+            ],
+            cwd=cargo_root,
+        )
+        self.children.append(p)
+        return p, port
+
+    def start_ssh_client(self, *args, password=None):
+        preargs = []
+        if password:
+            preargs = ['sshpass', '-p', password]
+        p = subprocess.Popen(
+            [
+                *preargs,
+                'ssh',
+                # '-v',
+                '-o',
+                'StrictHostKeychecking=no',
+                '-o',
+                'UserKnownHostsFile=/dev/null',
+                *args,
+            ],
+            stdout=subprocess.PIPE,
+        )
+        self.children.append(p)
+        return p
 
 
-def start_ssh_client(*args, password=None):
-    preargs = []
-    if password:
-        preargs = ['sshpass', '-p', password]
-    p = subprocess.Popen(
-        [
-            *preargs,
-            'ssh',
-            # '-v',
-            '-o',
-            'StrictHostKeychecking=no',
-            '-o',
-            'UserKnownHostsFile=/dev/null',
-            *args,
-        ],
-    )
-    children.append(p)
-    return p
-
-
-logging.basicConfig(level=logging.DEBUG)
 wg_c_ed25519_pubkey = Path(os.getcwd()) / 'ssh-keys/wg/client-ed25519.pub'
 
 
+@pytest.fixture
+def ctx():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        ctx = Context(tmpdir=Path(tmpdir))
+        yield ctx
+
+
+@pytest.fixture()
+def processes(ctx):
+    mgr = ProcessManager(ctx)
+    try:
+        yield mgr
+    finally:
+        mgr.stop()
+
+
 class TestClass:
-    def test_ssh_password_auth(self):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            ctx = Context(tmpdir=Path(tmpdir))
-            print(f'Working in {ctx.tmpdir}')
-            try:
-                ssh_port = start_ssh_server(ctx, trusted_keys=[wg_c_ed25519_pubkey.read_text()])
-                wait_port(ssh_port)
-                wg_port = start_wg(
-                    ctx,
-                    dedent(
-                        f'''\
-                        targets:
-                        -   name: ssh
-                            allow_roles: [role]
-                            ssh:
-                                host: localhost
-                                port: {ssh_port}
-                                username: {os.getlogin()}
-                        users:
-                        -   username: user
-                            roles: [role]
-                            credentials:
-                            -   type: password
-                                hash: '$argon2id$v=19$m=4096,t=3,p=1$cxT6YKZS7r3uBT4nPJXEJQ$GhjTXyGi5vD2H/0X8D3VgJCZSXM4I8GiXRzl4k5ytk0' # 123
-                        '''
-                    ),
-                )
-                wait_port(wg_port)
-                logging.info('running')
-                ssh_client = start_ssh_client(
-                    'user:ssh@localhost',
-                    '-p',
-                    str(wg_port),
-                    '-i',
-                    '/dev/null',
-                    '-o',
-                    'PreferredAuthentications=password',
-                    'ls',
-                    password='123',
-                )
-                print(ssh_client.communicate())
-                print(ssh_client.returncode)
-                assert ssh_client.returncode == 0
-            finally:
-                for p in children:
-                    p.terminate()
-                for p in children:
-                    p.kill()
+    def test_ssh_conntest_success(self, processes: ProcessManager):
+        ssh_port = processes.start_ssh_server(
+            trusted_keys=[wg_c_ed25519_pubkey.read_text()]
+        )
+        wait_port(ssh_port)
+        proc, _ = processes.start_wg(
+            config=dedent(
+                f'''\
+                users: []
+                targets:
+                -   name: ssh
+                    allow_roles: [role]
+                    ssh:
+                        host: localhost
+                        port: {ssh_port}
+                        username: {os.getlogin()}
+                '''
+            ),
+            args=['test-target', 'ssh']
+        )
+        proc.wait(timeout=5)
+        assert proc.returncode == 0
 
-        # cargo llvm-cov --no-run --open
+    def test_ssh_password_auth(self, processes: ProcessManager):
+        ssh_port = processes.start_ssh_server(
+            trusted_keys=[wg_c_ed25519_pubkey.read_text()]
+        )
+        wait_port(ssh_port)
+        _, wg_port = processes.start_wg(
+            dedent(
+                f'''\
+                targets:
+                -   name: ssh
+                    allow_roles: [role]
+                    ssh:
+                        host: localhost
+                        port: {ssh_port}
+                        username: {os.getlogin()}
+                users:
+                -   username: user
+                    roles: [role]
+                    credentials:
+                    -   type: password
+                        hash: '$argon2id$v=19$m=4096,t=3,p=1$cxT6YKZS7r3uBT4nPJXEJQ$GhjTXyGi5vD2H/0X8D3VgJCZSXM4I8GiXRzl4k5ytk0' # 123
+                '''
+            ),
+        )
+        wait_port(wg_port)
+        logging.info('running')
+        ssh_client = processes.start_ssh_client(
+            'user:ssh@localhost',
+            '-p',
+            str(wg_port),
+            '-i',
+            '/dev/null',
+            '-o',
+            'PreferredAuthentications=password',
+            'ls',
+            '/bin/sh',
+            password='123',
+        )
+        assert ssh_client.communicate()[0] == b'/bin/sh\n'
+        assert ssh_client.returncode == 0
 
-TestClass().test_ssh_password_auth()
+
+# cargo llvm-cov --no-run --open
+
+# TestClass().test_ssh_password_auth()
