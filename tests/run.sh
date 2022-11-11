7479f1eee617d0e2a352dfcaec0b664fe58b0b05
Eugene Pankov <e@ajenti.org>
2022-11-11 14:03:55 +0100

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -5,5 +5,5 @@ rm target/llvm-cov-target/* || true
 cargo llvm-cov clean --workspace
 cargo llvm-cov --no-report --workspace --all-features -- --skip agent
 cd tests
-RUST_BACKTRACE=1 poetry run pytest -s $@
+RUST_BACKTRACE=1 poetry run pytest $@
 cargo llvm-cov --no-run --hide-instantiations --html
