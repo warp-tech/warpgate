f0bc1db9aeaa3c5d182dcc6c4cc06aa2186e2c5c
Eugene Pankov <inbox@null.page>
2023-08-07 22:36:03 +0200

    fixed #358 - quotes in connection instructions on Windows

---
+++ warpgate-web/src/gateway/lib/shellEscape.ts
@@ -0,0 +1,32 @@
+import { UAParser } from 'ua-parser-js'
+
+function escapeUnix (arg: string): string {
+    if (!/^[A-Za-z0-9_\/-]+$/.test(arg)) {
+        return ('\'' + arg.replace(/'/g, '\'"\'"\'') + '\'').replace(/''/g, '')
+    }
+    return arg
+}
+
+function escapeWin (arg: string): string {
+    if (!/^[A-Za-z0-9_\/-]+$/.test(arg)) {
+        return '"' + arg.replace(/"/g, '""') + '"'
+    }
+    return arg
+}
+
+const isWin = new UAParser().getOS().name === 'Windows'
+
+export function shellEscape (stringOrArray: string[]|string): string {
+    const ret = []
+
+    const escapePath = isWin ? escapeWin : escapeUnix
+
+    if (typeof stringOrArray == 'string') {
+        return escapePath(stringOrArray)
+    } else {
+        stringOrArray.forEach(function (member) {
+            ret.push(escapePath(member))
+        })
+        return ret.join(' ')
+    }
+}
