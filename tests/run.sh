eee486a5966c2e9a9d05d487c1674317f5d450b6
Eugene <inbox@null.page>
2024-01-23 12:20:19 +0100

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -3,7 +3,7 @@ set -e
 cd ..
 rm target/llvm-cov-target/* || true
 cargo llvm-cov clean --workspace
-cargo llvm-cov --no-report --workspace --all-features -- --skip agent
+cargo llvm-cov --no-cfg-coverage-nightly --no-report --workspace --all-features -- --skip agent
 cd tests
 RUST_BACKTRACE=1 poetry run pytest $@
 cargo llvm-cov --no-run --hide-instantiations --html
