81cefebe967e36b7db9f9ad12f30ae0b61e3cf8b
Eugene <inbox@null.page>
2024-03-23 22:03:18 +0100

    fixed #966 - don't try to change config permissions unless necessary

--- warpgate-common/src/helpers/fs.rs
+++ warpgate-common/src/helpers/fs.rs
@@ -1,10 +1,21 @@
 use std::os::unix::prelude::PermissionsExt;
 use std::path::Path;
 
+fn maybe_apply_permissions<P: AsRef<Path>>(
+    path: P,
+    permissions: std::fs::Permissions,
+) -> std::io::Result<()> {
+    let current = std::fs::metadata(&path)?.permissions();
+    if current != permissions {
+        std::fs::set_permissions(path, permissions)?;
+    }
+    Ok(())
+}
+
 pub fn secure_directory<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
-    std::fs::set_permissions(path.as_ref(), std::fs::Permissions::from_mode(0o700))
+    maybe_apply_permissions(path.as_ref(), std::fs::Permissions::from_mode(0o700))
 }
 
 pub fn secure_file<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
-    std::fs::set_permissions(path.as_ref(), std::fs::Permissions::from_mode(0o600))
+    maybe_apply_permissions(path.as_ref(), std::fs::Permissions::from_mode(0o600))
 }
