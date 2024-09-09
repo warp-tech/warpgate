dacbe04f630e9bb4751eb79c072291fe756782e3
kekkon <ketsukonn@gmail.com>
2024-09-10 01:41:50 +0200

    Password and TOTP validation

--- warpgate-web/src/admin/UserCredentialModal.svelte
+++ warpgate-web/src/admin/UserCredentialModal.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
 import { onMount } from 'svelte'
-import { Alert, Button, FormGroup, Input, Modal, ModalBody, ModalFooter, ModalHeader } from '@sveltestrap/sveltestrap'
+import {
+    Alert,
+    Button,
+    FormGroup,
+    Input,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+} from '@sveltestrap/sveltestrap'
 import QRCode from 'qrcode'
-import { TOTP, type TOTPOptions } from '@otplib/core'
-import { createDigest } from '@otplib/plugin-crypto-js'
-import { faRefresh } from '@fortawesome/free-solid-svg-icons'
+import * as OTPAuth from 'otpauth'
+import { faClipboard, faRefresh } from '@fortawesome/free-solid-svg-icons'
 import Fa from 'svelte-fa'
 import base32Encode from 'base32-encode'
 
 import { api } from 'gateway/lib/api'
-import type { UserAuthCredential } from './lib/api'
+import type { UserAuthCredential, UserTotpCredential } from './lib/api'
 
 export let credential: UserAuthCredential
 export let username: string
@@ -19,12 +27,21 @@ let visible = true
 let newPassword = ''
 let field: HTMLInputElement|undefined
 let qrImage: HTMLImageElement|undefined
+let totpUri: string|undefined
+let totpValidationValue: string|undefined
+let validationFeedback: string|undefined
+let totpValid = false
+let passwordValid = false
 
-export const totp = new TOTP<TOTPOptions>({
-    createDigest,
+export const totp = new OTPAuth.TOTP({
+    issuer: 'Warpgate',
+    digits: 6,
+    period: 30,
+    algorithm: 'SHA1',
 })
 
 function _save () {
+
     if (credential.kind === 'Password') {
         if (!newPassword) {
             return
@@ -41,10 +58,52 @@ function _save () {
     save()
 }
 
+function _validate () : boolean {
+    console.debug(`Validating credentials of kind "${credential.kind}"`)
+
+    if (credential.kind === 'Totp' && totpValidationValue) {
+        totp.secret ??= OTPAuth.Secret.fromBase32(generateTotpSecret(credential))
+        totpValid = totp.validate({ token: totpValidationValue, window: 1 }) !== null
+
+        if (!totpValid) {
+            validationFeedback = 'The TOTP code is not valid'
+        } else {
+            validationFeedback = undefined
+        }
+
+        return totpValid
+    } else if (credential.kind === 'Password') {
+        passwordValid = newPassword.trim().length > 1
+
+        if (!passwordValid) {
+            validationFeedback = 'Password cannot be empty or whitespace'
+        }
+
+        return passwordValid
+    } else {
+        // TODO: Further validation
+        return true
+    }
+}
+
 function generateNewTotpKey () {
     if (credential.kind === 'Totp') {
         credential.key = Array.from({ length: 32 }, () => Math.floor(Math.random() * 255))
     }
+}
+
+/**
+ * Copies the TOTP URI to the system clipboard if it is defined.
+ *
+ * @return {Promise<void>} A promise that resolves when the TOTP URI has been copied to the clipboard.
+ */
+async function copyTotpUri () : Promise<void> {
+    if (totpUri === undefined) {
+        return
+    }
+
+    const { clipboard } = navigator
+    return clipboard.writeText(totpUri)
 }
 
 function _cancel () {
@@ -58,15 +117,28 @@ onMount(() => {
     })
 })
 
+
+/**
+ * Generates a TOTP (Time-based One-Time Password) secret key encoded in base32.
+ *
+ * @param {UserTotpCredential} cred - The credential containing a key for TOTP generation.
+ * @return {string} The base32 encoded TOTP secret key.
+ */
+function generateTotpSecret (cred: UserTotpCredential) : string {
+    return base32Encode(new Uint8Array(cred.key), 'RFC4648')
+}
+
 $: {
     if (credential.kind === 'Totp') {
         if (!credential.key.length) {
             generateNewTotpKey()
         }
 
-        const uri = totp.keyuri(username, 'Warpgate', base32Encode(new Uint8Array(credential.key), 'RFC4648'))
+        totp.label = username
+        totp.secret = OTPAuth.Secret.fromBase32(generateTotpSecret(credential))
+        totpUri = totp.toString()
 
-        QRCode.toDataURL(uri, (err: Error | null | undefined, imageUrl: string) => {
+        QRCode.toDataURL(totpUri, (err: Error | null | undefined, imageUrl: string) => {
             if (err) {
                 return
             }
@@ -75,6 +147,8 @@ $: {
             }
         })
     }
+
+    _validate()
 }
 </script>
 
@@ -123,11 +197,17 @@ $: {
         {/if}
 
         {#if credential.kind === 'Password'}
-            <Input
-                bind:inner={field}
-                type="password"
-                placeholder="New password"
-                bind:value={newPassword} />
+            <FormGroup floating class="mt-3" label="Enter a new password">
+                <Input
+                    bind:inner={field}
+                    bind:feedback={validationFeedback}
+                    type="password"
+                    placeholder="New password"
+                    valid={passwordValid}
+                    invalid={!passwordValid}
+                    on:change={_validate}
+                    bind:value={newPassword} />
+            </FormGroup>
         {/if}
 
         {#if credential.kind === 'PublicKey'}
@@ -142,21 +222,29 @@ $: {
         {#if credential.kind === 'Totp'}
             <div class="row">
                 <div class="col-12 col-md-6">
-                    <img class="qr" bind:this={qrImage} alt="OTP QR code" />
+                    <img class="qr" id="totpQR" bind:this={qrImage} alt="OTP QR code" />
                 </div>
                 <div class="col-12 col-md-6">
-                    <Button outline class="d-flex align-items-center" color="primary" on:click={generateNewTotpKey}>
+                    <Button outline class="d-flex align-items-center" color="heading" on:click={generateNewTotpKey}>
                         <Fa class="me-2" fw icon={faRefresh} />
                         Reset secret key
                     </Button>
+                    <Button outline class="d-flex align-items-center" color="heading" on:click={copyTotpUri}>
+                        <Fa class="me-2" fw icon={faClipboard} />
+                        Copy raw value
+                    </Button>
                 </div>
             </div>
+            <FormGroup floating label="Paste TOTP code for validation" class="mt-3">
+                <Input required bind:feedback={validationFeedback} bind:value={totpValidationValue} valid={totpValid} invalid={!totpValid} on:change={_validate} />
+            </FormGroup>
         {/if}
     </ModalBody>
     <ModalFooter>
         <div class="d-flex">
             <Button
                 class="ms-auto"
+                disabled={!_validate}
                 outline
                 on:click={_save}
             >Save</Button>
