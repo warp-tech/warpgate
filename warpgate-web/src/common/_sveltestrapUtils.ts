b37f907ec7ac6206c892871e15b759a53dd11482
Eugene <inbox@null.page>
2024-11-24 20:41:52 +0100

    fixed tooltips

--- warpgate-web/src/common/_sveltestrapUtils.ts
+++ warpgate-web/src/common/_sveltestrapUtils.ts
@@ -23,3 +23,12 @@ export function toClassName(value: any) {
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const classnames = (...args: any[]) => args.map(toClassName).filter(Boolean).join(' ')
+
+
+export function uuid(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        const r = (Math.random() * 16) | 0
+        const v = c === 'x' ? r : (r & 0x3) | 0x8
+        return v.toString(16)
+    })
+}
