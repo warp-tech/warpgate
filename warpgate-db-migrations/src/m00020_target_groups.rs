856ba122324aae023297572b65903748381beea3
Skyler Lewis <skyler.lewis@tavahealth.com>
2025-11-16 15:14:36 -0700

    use existing pattern for adding entities to migrations

--- warpgate-db-migrations/src/m00020_target_groups.rs
+++ warpgate-db-migrations/src/m00020_target_groups.rs
@@ -1,4 +1,28 @@
+use sea_orm::Schema;
 use sea_orm_migration::prelude::*;
+
+use crate::m00007_targets_and_roles::target;
+
+pub mod target_group {
+    use sea_orm::entity::prelude::*;
+    use uuid::Uuid;
+
+    #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+    #[sea_orm(table_name = "target_groups")]
+    pub struct Model {
+        #[sea_orm(primary_key, auto_increment = false)]
+        pub id: Uuid,
+        pub name: String,
+        #[sea_orm(column_type = "Text")]
+        pub description: String,
+        pub color: Option<String>,
+    }
+
+    #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+    pub enum Relation {}
+
+    impl ActiveModelBehavior for ActiveModel {}
+}
 
 pub struct Migration;
 
@@ -12,43 +36,19 @@ impl MigrationName for Migration {
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Create target_groups table
+        let builder = manager.get_database_backend();
+        let schema = Schema::new(builder);
         manager
-            .create_table(
-                Table::create()
-                    .table(TargetGroups::Table)
-                    .if_not_exists()
-                    .col(
-                        ColumnDef::new(TargetGroups::Id)
-                            .uuid()
-                            .not_null()
-                            .primary_key(),
-                    )
-                    .col(
-                        ColumnDef::new(TargetGroups::Name)
-                            .string()
-                            .not_null(),
-                    )
-                    .col(
-                        ColumnDef::new(TargetGroups::Description)
-                            .text()
-                            .not_null(),
-                    )
-                    .col(
-                        ColumnDef::new(TargetGroups::Color)
-                            .string()
-                            .null(),
-                    )
-                    .to_owned(),
-            )
+            .create_table(schema.create_table_from_entity(target_group::Entity))
             .await?;
 
         // Add group_id column to targets table
         manager
             .alter_table(
                 Table::alter()
-                    .table(Targets::Table)
+                    .table(target::Entity)
                     .add_column(
-                        ColumnDef::new(Targets::GroupId)
+                        ColumnDef::new(Alias::new("group_id"))
                             .uuid()
                             .null(),
                     )
@@ -67,36 +67,17 @@ impl MigrationTrait for Migration {
         manager
             .alter_table(
                 Table::alter()
-                    .table(Targets::Table)
-                    .drop_column(Targets::GroupId)
+                    .table(target::Entity)
+                    .drop_column(Alias::new("group_id"))
                     .to_owned(),
             )
             .await?;
 
         // Drop target_groups table
         manager
-            .drop_table(
-                Table::drop()
-                    .table(TargetGroups::Table)
-                    .to_owned(),
-            )
+            .drop_table(Table::drop().table(target_group::Entity).to_owned())
             .await?;
 
         Ok(())
     }
-}
-
-#[derive(DeriveIden)]
-enum TargetGroups {
-    Table,
-    Id,
-    Name,
-    Description,
-    Color,
-}
-
-#[derive(DeriveIden)]
-enum Targets {
-    Table,
-    GroupId,
 }
