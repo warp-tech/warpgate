003aecd5ffd57ed056b295b9068102f058dcb5f3
Lars <60571459+LarsSven@users.noreply.github.com>
2026-01-05 13:21:14 +0100

    Undo API-incompatible TlsMode change (#1648)
    Co-authored-by: Eugene <inbox@null.page>

--- warpgate-tls/src/mode.rs
+++ warpgate-tls/src/mode.rs
@@ -4,23 +4,20 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize, Clone, Copy, Enum, PartialEq, Eq, Default)]
 pub enum TlsMode {
     #[serde(rename = "disabled")]
-    #[oai(rename = "disabled")]
     Disabled,
     #[serde(rename = "preferred")]
-    #[oai(rename = "preferred")]
     #[default]
     Preferred,
     #[serde(rename = "required")]
-    #[oai(rename = "required")]
     Required,
 }
 
 impl From<&str> for TlsMode {
     fn from(s: &str) -> Self {
         match s {
-            "disabled" => TlsMode::Disabled,
-            "preferred" => TlsMode::Preferred,
-            "required" => TlsMode::Required,
+            "Disabled" => TlsMode::Disabled,
+            "Preferred" => TlsMode::Preferred,
+            "Required" => TlsMode::Required,
             _ => TlsMode::Preferred,
         }
     }
@@ -29,9 +26,9 @@ impl From<&str> for TlsMode {
 impl From<TlsMode> for String {
     fn from(mode: TlsMode) -> Self {
         match mode {
-            TlsMode::Disabled => "disabled".to_string(),
-            TlsMode::Preferred => "preferred".to_string(),
-            TlsMode::Required => "required".to_string(),
+            TlsMode::Disabled => "Disabled".to_string(),
+            TlsMode::Preferred => "Preferred".to_string(),
+            TlsMode::Required => "Required".to_string(),
         }
     }
 }
