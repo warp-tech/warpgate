5b5cabf4e24b6729ca86f5c712656ba64feb7ac4
Eugene <inbox@null.page>
2026-09-16 09:02:15 +0200

    better handling for malformed external_host values

--- warpgate-protocol-http/src/api/sso_provider_detail.rs
+++ warpgate-protocol-http/src/api/sso_provider_detail.rs
@@ -75,11 +75,10 @@ impl Api {
         if matches!(
             provider_config.return_url_domain,
             SsoReturnUrlDomainPreference::ExternalHost
-        ) && let (Some(request_host), Some(external_host)) = (
-            ctx.trusted_hostname(req),
-            config.store.external_host.as_deref(),
-        ) && !is_localhost_host(&request_host)
-            && !host_is_subdomain_of_or_equal(&request_host, external_host)
+        ) && let (Some(request_host), Some(external_host)) =
+            (ctx.trusted_hostname(req), config.external_host_name())
+            && !is_localhost_host(&request_host)
+            && !host_is_subdomain_of_or_equal(&request_host, &external_host)
         {
             return Ok(StartSsoResponse::IncompatibleSsoDomain);
         }
